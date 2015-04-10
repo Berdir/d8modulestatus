3f749bd20b748b34437c948b053449fc486d046f
d8ms <info+d8ms@md-systems.ch>
2015-04-10 18:23:57 +0200

    Cleanup, include more test classes

--- build.sh
+++ build.sh
@@ -27,18 +27,10 @@ cd core
 /usr/local/bin/composer drupal-update
 cd ..
 
-# Define a mapping of non-standard test groups.
-declare -A groupMap
-groupMap[currency]=Currency
-groupMap[google_analytics]="Google Analytics"
-groupMap[payment]="Payment"
-
 # Loop over all projects.
 for FOLDER in `cd modules; ls -d1 */`; do
   # Remove trailing / from foldername
   PROJECT=${FOLDER%%/}
-  # Set simpletest group from the group map, default to project name.
-  GROUP=${groupMap[$PROJECT]:-$PROJECT}
 
   echo ""
   echo "=== Testing $PROJECT ==="
@@ -47,10 +39,9 @@ for FOLDER in `cd modules; ls -d1 */`; do
   # Prepare www folder.
   mkdir -p ../www-new/$PROJECT/simpletest
   ./core/vendor/bin/phpunit -c core/phpunit.xml.dist --log-junit=../www-new/$PROJECT/phpunit.xml modules/$PROJECT/
-  #php core/scripts/run-tests.sh --concurrency $CONCURRENCY --url http://d8modulestatus/ --xml ../www-new/$PROJECT/simpletest "$GROUP"
 done
 
-test_files=`find modules/ -name "*Test.php" | grep src/Tests | paste -s -d,`
+test_files=`find modules/ -name "*Test*.php" | grep src/Tests | paste -s -d,`
 
 mkdir -p ../www-new/simpletest
 php core/scripts/run-tests.sh --concurrency $CONCURRENCY --url http://d8modulestatus/ --xml ../www-new/simpletest --file "$test_files"
